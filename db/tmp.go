9979a2673cfd0d99168cfb6336eda34b35a54f88
Michael Stergianis <michaelstergianis@gmail.com>
2019-04-02 23:50:08 -0400

    Move reflection to tmp file. Hand written functions back to db.

--- db/tmp.go
+++ db/tmp.go
@@ -1,141 +1,37 @@
 package db
 
-import "database/sql"
+import "reflect"
 
-// GetUniqueArtist ...
-func (hdb *HeraldDB) GetUniqueArtist(artist Artist) (a Artist, err error) {
-	baseQuery := "SELECT id, name, fs_path FROM music.artists WHERE "
-
-	var row *sql.Row
-
-	if artist.ID != 0 {
-		row = hdb.QueryRow(baseQuery+"artists.id = $1", artist.ID)
-	} else if artist.Path != "" {
-		row = hdb.QueryRow(baseQuery+"artists.fs_path = $1", artist.Path)
-	} else {
-		return Artist{}, ErrNonUnique
+// prepareDest ...
+func prepareDest(rdest *reflect.Value) (destArr []interface{}) {
+	rdestVal := rdest.Elem()
+	destArr = make([]interface{}, rdestVal.NumField())
+	for i := 0; i < rdestVal.NumField(); i++ {
+		destArr[i] = rdestVal.Field(i).Addr().Interface()
 	}
-
-	err = row.Scan(&a.ID, &a.Name, &a.Path)
-
-	if err == sql.ErrNoRows {
-		return Artist{}, ErrNotPresent
-	}
-
-	if err != nil {
-		return Artist{}, err
-	}
-
-	return a, nil
+	return destArr
 }
 
-// GetUniqueGenre ...
-func (hdb *HeraldDB) GetUniqueGenre(genre Genre) (g Genre, err error) {
-	baseQuery := "SELECT id, name " +
-		"FROM music.genres WHERE "
-
-	var row *sql.Row
-
-	if genre.ID != 0 {
-		row = hdb.QueryRow(baseQuery+"genres.id = $1", genre.ID)
-	} else if genre.Name != "" {
-		row = hdb.QueryRow(baseQuery+"genres.name = $1", genre.Name)
-	} else {
-		return Genre{}, ErrNonUnique
+// GetUniqueItem ...
+// Returns a unique item from the database. Requires an id.
+func (hdb *HeraldDB) GetUniqueItem(table string, query interface{}, dest interface{}) (err error) {
+	if !isValidTable(table) {
+		return ErrInvalidTable
+	}
+	rquery := reflect.ValueOf(query)
+	rdest := reflect.ValueOf(dest)
+	if rdest.Kind() != reflect.Ptr || rdest.IsNil() {
+		return ErrReflection
 	}
 
-	err = row.Scan(
-		&g.ID,
-		&g.Name,
-	)
+	q, a := prepareUniqueQuery(table, rquery)
 
-	if err == sql.ErrNoRows {
-		return Genre{}, ErrNotPresent
-	}
+	destArr := prepareDest(&rdest)
 
+	err = hdb.QueryRow(q, a...).Scan(destArr...)
 	if err != nil {
-		return Genre{}, err
+		return err
 	}
 
-	return g, nil
-}
-
-// GetUniqueSong ...
-func (hdb *HeraldDB) GetUniqueSong(song Song) (s Song, err error) {
-	baseQuery := "SELECT id, album, genre, fs_path, title, track, num_tracks, disk, num_disks, song_size, duration, artist " +
-		"FROM music.songs WHERE "
-
-	var row *sql.Row
-
-	if song.ID != 0 {
-		row = hdb.QueryRow(baseQuery+"songs.id = $1", song.ID)
-	} else if song.Path != "" {
-		row = hdb.QueryRow(baseQuery+"songs.fs_path = $1", song.Path)
-	} else {
-		return Song{}, ErrNonUnique
-	}
-
-	err = row.Scan(
-		&s.ID,
-		&s.Album,
-		&s.Genre,
-		&s.Path,
-		&s.Title,
-		&s.Track,
-		&s.NumTracks,
-		&s.Disk,
-		&s.NumDisks,
-		&s.Size,
-		&s.Duration,
-		&s.Artist,
-	)
-
-	if err == sql.ErrNoRows {
-		return Song{}, ErrNotPresent
-	}
-
-	if err != nil {
-		return Song{}, err
-	}
-
-	return s, nil
-}
-
-// GetUniqueAlbum ...
-// Returns a full album based on some unique information.
-// Accepted fields
-func (hdb *HeraldDB) GetUniqueAlbum(album Album) (a Album, err error) {
-	baseQuery := "SELECT id, artist, release_year, n_tracks, n_disks, title, fs_path, duration FROM music.albums WHERE "
-
-	var row *sql.Row
-
-	if album.ID != 0 {
-		row = hdb.QueryRow(baseQuery+"albums.id = $1", album.ID)
-	} else if album.Path != "" {
-		row = hdb.QueryRow(baseQuery+"albums.fs_path = $1", album.Path)
-	} else {
-		return Album{}, ErrNonUnique
-	}
-
-	err = row.Scan(
-		&a.ID,
-		&a.Artist,
-		&a.Year,
-		&a.NumTracks,
-		&a.NumDisks,
-		&a.Title,
-		&a.Path,
-		&a.Duration,
-	)
-
-	if err == sql.ErrNoRows {
-		return Album{}, ErrNotPresent
-	}
-
-	// error scanning the row
-	if err != nil {
-		return Album{}, err
-	}
-
-	return a, nil
+	return nil
 }
